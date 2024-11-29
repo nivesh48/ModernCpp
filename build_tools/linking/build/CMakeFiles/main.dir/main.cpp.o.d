47eedcf1c512b5a24d38d0b5483eeaa6bf69ac38
Nivesh Gadipudi <nivesh48@gmail.com>
2024-11-29 14:50:21 +0000

    cmake build example

---
+++ build_tools/linking/build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/main.cpp.o: \
+ /mnt/raid5/gadipudn/capture_stuff/ModernCpp/build_tools/linking/main.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/raid5/gadipudn/capture_stuff/ModernCpp/build_tools/linking/some_file.hpp
